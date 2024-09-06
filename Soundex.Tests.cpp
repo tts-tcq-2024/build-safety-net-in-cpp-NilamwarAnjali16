e22cf2c61c26ee3bbf81247b77460b1974714108
NilamwarAnjali16 <nilamwaranjali@gmail.com>
2024-09-06 09:19:18 +0530

    LFI2KOR_Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,12 +1,34 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-
-TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "");
+TEST(SoundexTest, BasicTest) {
+    EXPECT_EQ(createSoundex("Smith"), "S530");
+    EXPECT_EQ(createSoundex("Smyth"), "S530");
+    EXPECT_EQ(createSoundex("Robert"), "R163");
+    EXPECT_EQ(createSoundex("Rupert"), "R163");
+    EXPECT_EQ(createSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(createSoundex("Ashcroft"), "A261");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
+TEST(SoundexTest, EdgeCases) {
+    EXPECT_EQ(createSoundex("Chand"), "C530");
+    EXPECT_EQ(createSoundex("Mehra"), "M600");
+    EXPECT_EQ(createSoundex("Pandey"), "P530");
+    EXPECT_EQ(createSoundex(""), "");
 }
+
+TEST(SoundexTest, CaseInsensitive) {
+    EXPECT_EQ(createSoundex("Robert"), createSoundex("robert"));
+    EXPECT_EQ(createSoundex("Smith"), createSoundex("sMiTh"));
+}
+TEST(SoundexTest, SingleCharacterName) {
+    EXPECT_EQ(createSoundex("A"), "A000");
+    EXPECT_EQ(createSoundex("B"), "B000");
+}
+
+TEST(SoundexTest, AllVowels) {
+    EXPECT_EQ(createSoundex("Aeio"), "A000");
+    EXPECT_EQ(createSoundex("Euio"), "E000");
+}
+
 
